9ff2b7e188f29135a293da0ebdca686145bc354c
Anil K Rajamoni <anil.k.rajamoni99@gmail.com>
2023-02-04 12:50:10 +0530

    added chapter 4 concepts

--- Chapter-301/PkgExamples/Simple.java
+++
@@ -1,5 +0,0 @@
-public class Simple{  
- public static void main(String args[]){  
-    System.out.println("Welcome to package");  
-   }  
-}  
--- Chapter-301/PkgExamples/pack2/Pack2Demo.java
+++ Chapter-301/PkgExamples/pack2/Pack2Demo.java
@@ -1,11 +1,11 @@
 package pack2;
 
+
 public class Pack2Demo {
 public static void main(String[] args) {
 
-  SimpleCalculator s = new SimpleCalculator();
-
-  System.out.println(s.add(10,20));
+  pack1.API api = new pack1.API();
+  api.deleteMethod();
 
   }
 }
