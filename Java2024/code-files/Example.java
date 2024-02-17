2bba5bfc48ef723076674500f1368d244b13beaf
anil-k-rajamoni18 <anilkrajamoni99@gmail.com>
2024-02-17 21:33:41 +0530

    added files

---
+++ Java2024/code-files/Example.java
@@ -0,0 +1,35 @@
+public class Example {
+
+    // static variable
+    private final static String collegeName = "SRI INDIU";
+
+    // declare instance variables 
+    public String name;
+    public float collegeFee;
+
+    public static void main(String[] args) {
+        // delcare two local variables 
+        int num1 = 100;
+        int num2 = 300;
+        System.out.println("Addition : "+ (num1 + num2));
+        System.out.println("Multiplication: "+ (num1 * num2));
+
+        // Accessing instance variables with object name 
+        Example e1 = new Example();
+        e1.name = "kumar";
+        e1.collegeFee = 182282.272F;
+
+        System.out.println(e1.name);
+        System.out.println(e1.collegeFee);
+
+        Example e2 = new Example();
+        e2.name = "srikanth";
+        e2.collegeFee = 182282.272F;
+
+        System.out.println(e2.name);
+        System.out.println(e2.collegeFee);
+        
+        // Accessing static variable with ClassName 
+        System.out.println(Example.collegeName);
+    }
+}
--- Java2024/code-files/Welcome.java
+++ Java2024/code-files/Welcome.java
@@ -1,2 +1,21 @@
+import java.util.Date; // import all classes of package
+import java.io.File;
+import java.io.IOException;
 public class Welcome {
+	
+	public static void main(String[] args) throws IOException // Main driver method
+	{
+		System.out.println("Welcome to Java World"); // Print statement
+		Date now  = new Date();
+		System.out.println("Current Date : " + now);
+		
+		// file creation 
+		File file = new File("C:\\Users\\akraj\\OneDrive\\Desktop\\Java2024\\addition.py");
+		
+		boolean isCreated = file.createNewFile(); // creates new files
+		System.out.println(" Is File created " + isCreated);
+		
+	}
+	
+	
 }
