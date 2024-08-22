ae9c002cc519351b45ea6fc976afd15edb3a7362
= <=>
2024-08-22 06:18:05 +0530

    new change

---
+++ Java_Training_2024/InputInJavaExample.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class InputInJavaExample {
+    public static void main(String[] args) {
+
+        // Command Line Arguments 
+        System.out.println(args.length);
+        System.out.println(args[0] + args[1] + args[2]);
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]) + Integer.parseInt(args[2]));
+
+
+        
+        /*
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter College Name: ");
+        
+        // String without whitespace
+        String collegeName = scan.next();
+        scan.nextLine(); // to read that line completely
+
+     
+
+        // String with whitespace
+        System.out.println("Enter Student Name: ");
+        String name = scan.nextLine();
+
+     
+        // byte 
+        System.out.println("Enter Semester No: ");
+        byte semesterNumber = scan.nextByte();
+
+   
+        // int 
+        System.out.println("Enter Student Age: ");
+        int age = scan.nextInt();
+
+        // long 
+        System.out.println("Enter Roll Num: ");
+        long rollNum = scan.nextLong();
+
+        // boolean 
+        System.out.println("Are you student? ");
+        boolean isStudent = scan.nextBoolean();
+
+        // double
+        System.out.println("Enter Fee: ");
+        double fee = scan.nextDouble();
+
+        // char 
+        System.out.println("Enter Grade: ");
+        char grade = scan.next().charAt(0);
+
+        System.out.println("CollegeName: " + collegeName);
+        System.out.println("StudentName: " + name);
+        System.out.println("Semester: " + semesterNumber);
+        System.out.println("age: " + age);
+        System.out.println("roll number: " + rollNum);
+        System.out.println("isStudent: " + isStudent);
+        System.out.println("fee: " + fee);
+        System.out.println("grade: " + grade);
+
+        */
+
+        
+    }
+}
