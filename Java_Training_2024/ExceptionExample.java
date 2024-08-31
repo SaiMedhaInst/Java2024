d28ec061c8887454788c76a4a81c56df1e713e7e
= <=>
2024-08-31 14:44:55 +0530

    new file added 31st-august-2024

--- Java_Training_2024/ExceptionExample.java
+++ Java_Training_2024/ExceptionExample.java
@@ -1,26 +1,16 @@
+import java.io.*;
+import java.sql.SQLException;
+
 public class ExceptionExample {
 
   public static void main(String[] args) {
     try {
-      System.out.println("Outer try block");
+      System.out.println("In try block");
+      throw new Error("Error in try");
+  } finally {
+      System.out.println("In finally block");
+      throw new Error("Error in finally");
+  }
 
-      try {
-        System.out.println("Inner try block");
-        int result = 10 / 0;
-        System.out.println(result);
-  
-      } finally {
-        System.out.println("inner finally block");
-      }
-      
-      int[] numbers = new int[5];
-      numbers[10] = 25;
-
-    } catch (ArithmeticException e) {
-      System.out.println("Caught ArithmeticException in outer catch block: " 
-      + e.getMessage());
-    }
-
-    System.out.println("After the nested try-catch blocks");
   }
 }
